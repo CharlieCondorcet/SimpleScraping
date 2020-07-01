1fbad4b11a06d02df8d23a81635b92903935083c
CharlieCondorcet <charliecondorcet@gmail.com>
2020-06-30 21:32:55 -0400

    feat(Functionary): add funcionary like model to ORM

---
+++ src/main/java/cl/ucn/disc/pdis/simplescraper/model/Functionary.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2020. Charlie Condorcet Engineering Student
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Eclipse Public License 2.0 which is available at
+ *  http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ *  which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *  SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
+package cl.ucn.disc.pdis.simplescraper.model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * The Functionary model class.
+ *
+ * @author Charlie Condorcet.
+ */
+@DatabaseTable(tableName = "functionary")
+public class Functionary {
+
+    /**
+     * The id: Primary Key.
+     */
+    @DatabaseField(canBeNull = false, id = true)
+    private int id;
+
+    /**
+     * The Nombre.
+     */
+    @DatabaseField(canBeNull = false, index = true)
+    private String nombre;
+
+    /**
+     * The Cargo.
+     */
+    @DatabaseField(canBeNull = false)
+    private String cargo;
+
+    /**
+     * The Unidad.
+     */
+    @DatabaseField(canBeNull = false)
+    private String unidad;
+
+    /**
+     * The E-Mail.
+     */
+    @DatabaseField(canBeNull = false)
+    private String email;
+
+    /**
+     * The Telefono.
+     */
+    @DatabaseField(canBeNull = false)
+    private String telefono;
+
+    /**
+     * The Oficina.
+     */
+    @DatabaseField(canBeNull = false)
+    private String oficina;
+
+    /**
+     * The Direccion.
+     */
+    @DatabaseField(canBeNull = false)
+    private String direccion;
+
+    /**
+     * Empty contructor; Default visivility + empty body.
+     */
+    public Functionary() {
+        // nothing here.
+    }
+
+    /**
+     * Principal Constructor.
+     *
+     * @param id
+     * @param nombre
+     * @param cargo
+     * @param unidad
+     * @param email
+     * @param telefono
+     * @param oficina
+     * @param direccion
+     */
+    public Functionary(int id, String nombre, String cargo, String unidad, String email, String telefono, String oficina, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.unidad = unidad;
+        this.email = email;
+        this.telefono = telefono;
+        this.oficina = oficina;
+        this.direccion = direccion;
+    }
+
+    /**
+     * Getter to ID.
+     *
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Getter to Nombre.
+     *
+     * @return nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Getter to Cargo.
+     *
+     * @return cargo
+     */
+    public String getCargo() {
+        return cargo;
+    }
+
+    /**
+     * Getter to Unidad.
+     *
+     * @return unidad
+     */
+    public String getUnidad() {
+        return unidad;
+    }
+
+    /**
+     * Getter to E-mail.
+     *
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Getter to Telefono.
+     *
+     * @return telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * Getter to Oficina.
+     *
+     * @return oficina
+     */
+    public String getOficina() {
+        return oficina;
+    }
+
+    /**
+     * Getter to Direccion.
+     *
+     * @return direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+}
